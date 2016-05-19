23c61c04987c9b58bc3a8e2eeabe9364b2f29537
Gomgom <contact@gom2.net>
2016-05-19 21:03:39 +0900

    first commit for o3yoccurinfo class.

--- pdairp/o3yoccurinfo.py
+++ pdairp/o3yoccurinfo.py
@@ -8,11 +8,10 @@ class O3YOccurInfo(object):
         self._service_key = str(imported_key)
         self._mainurl = 'http://openapi.airkorea.or.kr/openapi/services/rest/OzYlwsndOccrrncInforInqireSvc/'
 
-    def o3(self, station_name, searchCondition, page_no='1', num_of_rows='10'):
+    def o3occur(self, year, page_no='1', num_of_rows='10'):
         urllist = [
-            self._mainurl + 'getMsrstnAcctoLastDcsnDnsty?', 'stationName=', urllib.parse.quote(station_name),
-            "&searchCondition=", searchCondition, '&pageNo=', page_no, '&numOfRows=', num_of_rows,
-            '&ServiceKey=', self._service_key, '&_returnType=json'
+            self._mainurl + 'getOzAdvsryOccrrncInfo?', 'year=', year, '&pageNo=', page_no,
+            '&numOfRows=', num_of_rows, '&ServiceKey=', self._service_key, '&_returnType=json'
         ]
         url = ''.join(urllist)
 
@@ -25,8 +24,31 @@ class O3YOccurInfo(object):
         for i in range(0, returndata['totalCount']):
             returndata[str(i)] = dict()
             list = [
-                'dataTime', 'so2Avg', 'coAvg', 'o3Avg', 'no2Avg',
-                'pm10Avg'
+                'dataTime', 'districtName', 'moveName', 'issueTime', 'issueVal',
+                'clearTime', 'clearVal', 'maxVal'
+            ]
+            for j in list:
+                returndata[str(i)][j] = jsondata['list'][0][j]
+
+        return returndata
+
+    def yellowoccur(self, year, page_no='1', num_of_rows='10'):
+        urllist = [
+            self._mainurl + 'getYlwsndAdvsryOccrrncInfo?', 'year=', year, '&pageNo=', page_no,
+            '&numOfRows=', num_of_rows, '&ServiceKey=', self._service_key, '&_returnType=json'
+        ]
+        url = ''.join(urllist)
+
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        jsondata = json.loads(response)
+
+        returndata = dict()
+        returndata['totalCount'] = jsondata['totalCount']
+
+        for i in range(0, returndata['totalCount']):
+            returndata[str(i)] = dict()
+            list = [
+                'dataTime', 'tmCnt', 'tmArea'
             ]
             for j in list:
                 returndata[str(i)][j] = jsondata['list'][0][j]
