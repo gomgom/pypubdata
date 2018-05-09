67a5ebde99d61b55f54713bd4148b124f511345e
Gomgom <dev@gomgom.io>
2018-05-09 23:07:54 +0900

    Change version because of change upload system.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pypubdata',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     url='https://github.com/gomgomdev/pypubdata',
     download_url='https://github.com/gomgomdev/pypubdata/archive/master.zip',
