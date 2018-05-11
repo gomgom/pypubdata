748fbaa737b79b78989de021603571f91c9308b8
Gomgom <dev@gomgom.io>
2018-05-12 00:10:37 +0900

    Change setup.py for pypi.

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
     name='pypubdata',
-    version='0.1.4',
+    version='0.1.4.1',
     packages=find_packages(),
     url='https://github.com/gomgomdev/pypubdata',
     download_url='https://github.com/gomgomdev/pypubdata/archive/master.zip',
@@ -10,6 +11,7 @@ setup(
     author='Gomgom',
     author_email='dev@gomgom.io',
     description='It is simple module package for ROK Public Data OpenAPI.',
+    long_description=open('README.rst', 'rt', encoding='utf-8').read(),
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5'
